18e6356b1dd78490d586b1e17c119723b598c2ca
Tyler Dixon <onyxqk@gmail.com>
2023-09-17 23:25:08 -0400

    initial drawing functionality

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -7,27 +7,54 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class CanvasComponent {
   @ViewChild('canvasElement', { static: true }) canvas!: ElementRef<HTMLCanvasElement>;
-
-  private isResizing = false
-  private initialWidth: number
-  private initialHeight: number
+  private ctx: CanvasRenderingContext2D;
+  private isDrawing = false;
+  private isResizing = false;
+  private initialWidth: number;
+  private initialHeight: number;
+  private initialX: number;
+  private initialY: number;
 
   constructor() { }
 
+  ngAfterViewInit() {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+  }
+
+  startDrawing(event: MouseEvent) {
+    this.isDrawing = true
+    this.ctx.beginPath()
+    this.ctx.moveTo(event.clientX - this.canvas.nativeElement.getBoundingClientRect().left, event.clientY - this.canvas.nativeElement.getBoundingClientRect().top)
+  }
+
+  draw(event: MouseEvent) {
+    if (!this.isDrawing) return
+
+    this.ctx.lineTo(event.clientX - this.canvas.nativeElement.getBoundingClientRect().left, event.clientY - this.canvas.nativeElement.getBoundingClientRect().top)
+    this.ctx.stroke()
+  }
+
+  stopDrawing() {
+    this.isDrawing = false
+    this.ctx.closePath()
+  }
+
   startResize(event: MouseEvent) {
     this.isResizing = true;
-    this.initialWidth = this.canvas.nativeElement.offsetWidth;
-    this.initialHeight = this.canvas.nativeElement.offsetHeight;
+    this.initialWidth = this.canvas.nativeElement.offsetWidth
+    this.initialHeight = this.canvas.nativeElement.offsetHeight
+    this.initialX = event.clientX
+    this.initialY = event.clientY
   }
 
   resize(event: MouseEvent) {
-    if (!this.isResizing) return;
+    if (!this.isResizing) return
 
-    const newWidth = this.initialWidth + event.clientX - this.canvas.nativeElement.getBoundingClientRect().left
-    const newHeight = this.initialHeight + event.clientY - this.canvas.nativeElement.getBoundingClientRect().top
+    const deltaX = event.clientX - this.initialX
+    const deltaY = event.clientY - this.initialY
 
-    this.canvas.nativeElement.style.width = newWidth + 'px'
-    this.canvas.nativeElement.style.height = newHeight + 'px'
+    this.canvas.nativeElement.style.width = this.initialWidth + deltaX + 'px'
+    this.canvas.nativeElement.style.height = this.initialHeight + deltaY + 'px'
   }
 
   stopResize() {
