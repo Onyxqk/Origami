f8b205bc8fee2b765adbe8c2fac05d3b467ce04e
Tyler Dixon <onyxqk@gmail.com>
2023-12-03 13:00:40 -0500

    accessibility tests

--- cypress/e2e/origami.cy.ts
+++ cypress/e2e/origami.cy.ts
@@ -1,25 +1,45 @@
 describe('Scenario: Initial load', () => {
 
     describe('Given the user launches Origami' + ' When the page is loaded', () => {
-
-        // load Origami running on localhost
-        before(() => {
-            cy.visit('localhost:4200/')
-        })
-
         it('Then the title of the page is Origami', () => {
+            cy.visit('localhost:4200/')
             cy.title().should('eq', 'Origami')
         })
 
-        it('And the canvas is displayed', () => {
-            cy.get('#canvasElement').should('be.visible')
-        })
-
         it('And there are no detected accessibility violations', () => {
+            cy.visit('localhost:4200/')
             // inject Axe accessibility testing
             cy.injectAxe()
             // test the page for accessibility
-            cy.checkA11y()
+            cy.checkA11y(null,
+                {
+                    runOnly: {
+                      type: 'tag',
+                      values: ['wcag2a']
+                    }
+                  }
+            )
+        })
+    })
+})
+
+describe('Scenario: Opening Origami menu', () => {
+    describe('Given the user launches Origami' + ' When the user opens the Origami menu', () => {
+        it('Then there are no detected accessibility violations', () => {
+            cy.viewport(1500, 500)
+            cy.visit('localhost:4200/')
+            cy.get('.origami-button').click();
+            // inject Axe accessibility testing
+            cy.injectAxe()
+            // test the page for accessibility
+            cy.checkA11y(null,
+                {
+                    runOnly: {
+                      type: 'tag',
+                      values: ['wcag2a']
+                    }
+                  }
+            )
         })
     })
 })
